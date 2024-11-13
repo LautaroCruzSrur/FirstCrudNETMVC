4bfeaf308619273af3c4f0cb1cb22e7c3799fd10
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2024-11-12 23:47:01 -0300

    Agregar archivos de proyecto.

---
+++ AppCrud/Controllers/EmpleadoController.cs
@@ -0,0 +1,59 @@
+﻿using AppCrud.Data;
+using AppCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCrud.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private readonly AppDBContext _appDBContext;
+        public EmpleadoController(AppDBContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<Empleado> lista = await _appDBContext.Empleados.ToListAsync();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Nuevo(Empleado empelado)
+        {
+            await _appDBContext.Empleados.AddAsync(empelado);
+            await _appDBContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            Empleado empleado = await _appDBContext.Empleados.FirstAsync(e => e.IdEmpleado == id);
+            return View(empleado);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(Empleado empelado)
+        {
+            _appDBContext.Empleados.Update(empelado);
+            await _appDBContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            Empleado empleado = await _appDBContext.Empleados.FirstAsync(e => e.IdEmpleado == id);
+            _appDBContext.Empleados.Remove(empleado);
+            await _appDBContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+    }
+}
